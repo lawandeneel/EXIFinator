a8cd66b838e0a437b55797661f1d2da7884f09e0
mac <mac@macs-MacBook-Pro-4.local>
2017-03-17 21:48:10 -0400

    removing aws credentials

--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 
 #AWS Access Credentials
-AWS_ACCESS_KEY = "AKIAIITYFFKIZOPWNZLQ" 
-AWS_SECRET_KEY = "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" 
+AWS_ACCESS_KEY = <AWS_ACCESS_KEY> 
+AWS_SECRET_KEY = <AWS_SECRET_KEY> 
 
 #List of S3 Buckets to grab from
 s3_bucket_names = [
